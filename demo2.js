cda71b08e0d5aaa90d2336507b19497ac12d34a6
Shashank Shekhar <shashanknkework@gmail.com>
2022-06-21 18:02:16 +0530

    TagName

--- demo2.js
+++ demo2.js
@@ -15,13 +15,28 @@
 // addItem[0].style.color="Green";
 // addItem[0].style.fontWeight="700";
 
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[2]);
-items[2].style.backgroundColor="Green";
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[2]);
+// items[2].style.backgroundColor="Green";
 
-for(var i=0; i<items.length;i++){
-    items[i].style.fontWeight=800;
-}
+// for(var i=0; i<items.length;i++){
+//     items[i].style.fontWeight=800;
+
+
+// }-----------------GetElementby Tag Name
+var li=document.getElementsByTagName('li');
+//  console.log(li);
+//  console.log(li[1]);
+ console.log(li[4]);
+ li[4].style.color="Yellow";
+
+ //-------editing---
+
+ var cli=document.getElementsByClassName('list-group')
+ const newLi=cli.getElementsByTagName('li');
+ console.log(newLi);
+ 
+
 
 
